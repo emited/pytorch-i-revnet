bb3f4f39decab5f753071cc35de3164def1684da
cgeweb <conseil@cge-web.com>
2018-10-01 20:30:19 +0200

    changes

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import torch
 from models.iRevNet import iRevNet
 
 
-model = iRevNet(nBlocks=[6, 16, 72, 6], nStrides=[2, 2, 2, 2],
+model = iRevNet(nBlocks=[6], nStrides=[1],
                 nChannels=None, nClasses=1000, init_ds=2,
                 dropout_rate=0., affineBN=True, in_shape=[3, 224, 224],
                 mult=4)
